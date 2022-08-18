b0d58d1da9beb0289a06d35c41cf8dd499de0b45
Eddie <eddie@5xcampus.com>
2022-08-18 12:43:36 +0800

    add braintree payment

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'bootsnap', '>= 1.4.4', require: false
 gem 'paranoia', '~> 2.6'
 gem "kaminari", "~> 1.2"
 gem "aasm", "~> 5.3"
+gem "braintree", "~> 4.8.0"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_order, only: [:pay, :submit_payment]
 
   def new
     @order = Order.new
@@ -10,16 +11,48 @@ class OrdersController < ApplicationController
     order.price = 10
 
     if order.save
-      redirect_to "/", notice: "訂單建立成功"
       # 轉去刷卡頁面
+      redirect_to pay_order_path(id: order.serial), notice: "訂單建立成功，準備刷卡"
     else
       redirect_to plans_path, notice: "系統正在忙碌中，請稍候再試"
     end
   end
 
+  def pay
+    @token = gateway.client_token.generate
+  end
+
+  def submit_payment
+    result = gateway.transaction.sale(
+      amount: @order.price,
+      payment_method_nonce: params[:nonce]
+    )
+
+    if result.success?
+      @order.pay!
+      redirect_to "/", notice: "交易成功"
+    else
+      @order.fail!
+      redirect_to "/", notice: "交易失敗"
+    end
+  end
+
   private
+
+  def find_order
+    @order = Order.find_by!(serial: params[:id])
+  end
 
   def order_params
     params.require(:order).permit(:note)
+  end
+
+  def gateway
+    Braintree::Gateway.new(
+      environment: :sandbox,
+      merchant_id: '',
+      public_key: '',
+      private_key: '',
+    )
   end
 end
